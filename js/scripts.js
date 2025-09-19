c89a50377736adc6b216703d5f965264a9f16332
Carl Johansson <carlinspain@gmail.com>
2025-09-19 20:50:19 +0200

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+/* 
+ * jQuery
+ * global.js // SCRIPTS.JS ES COMO DECIR STYLE.CSS, PEOR USO GLOBAL.CSS ... 
+ */
+
+/*$(document).ready(function(){
+  $("#demo_wrapper_1").click(function(){
+    $("#demo_wrapper_2").slideToggle("slow");
+  });
+})*/
+
+$("header").click(function() {
+		// $(this).hide('slow');
+});
